4c9ea7cffa40255fcfd9c0c0e764334a396df0ac
aakashsell <aakashsell@gmail.com>
2024-10-06 18:15:51 -0400

    added more stuff

--- timing-algo/algo.cpp
+++ timing-algo/algo.cpp
@@ -1,17 +1,11 @@
 #include <pybind11/pybind11.h>
 
-int add(int i, int j) {
-    return i + j;
-}
 
-int sub(int i, int j) {
-    return i - j;
-}
+
+
 
 PYBIND11_MODULE(test, m) {
     m.doc() = "pybind11 example plugin"; // optional module docstring
-    m.def("add", &add, "A function that adds two numbers");
-    m.def("sub", &sub, "A function that subs two numbers");
 }
 
 
