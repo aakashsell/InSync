d8000127d31f14ec5dae22c38e959793750a4fcc
aakashsell <aakashsell@gmail.com>
2024-10-06 18:15:51 -0400

    added more stuff

--- timing-algo/main.py
+++ timing-algo/main.py
@@ -1,4 +1,28 @@
-import test
+import time
 
-print(test.add(3, 4))
-print(test.sub(3, 4))
+tempo = 60 #bpm
+second_per_beat = 60/tempo
+
+start_time = time.time()
+
+# Get sheet music data
+sheet_music = ['d1/4', 'e1/4', 'e1/2', 'e1/4', 'e1/4', 'e1/4']
+
+#transform sheet music into time series data.
+
+sheet_music = ['d', 'e', 'e', 'e', 'e', 'e', 'e']
+
+
+# Get Audio Data
+
+audio_data1 = [(0, 'd'), (1, 'e'), (2, 'e'), (4, 'e'), (5, 'e'), (6, 'e')]
+audio_data2 = [(0, 'd'), (2, 'e'), (2, 'e'), (4, 'e'), (5, 'e'), (6, 'e')]
+
+# compare data
+
+def compare(sheet, audio1, audio2):
+    #check where the data is in the music
+    
+
+
+
